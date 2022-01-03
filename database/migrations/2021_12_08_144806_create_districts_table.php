26c263b059a144f1e1215ef05e039e3861c6116b
haider8278 <haider8278@gmail.com>
2022-01-03 13:13:45 +0500

    fix migration issue

--- database/migrations/2021_12_08_144806_create_districts_table.php
+++ database/migrations/2021_12_08_144806_create_districts_table.php
@@ -17,7 +17,7 @@ class CreateDistrictsTable extends Migration
             $table->id();
             $table->string('name');
             $table->foreignId('region_id')->constrained();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id');
             $table->timestamps();
         });
     }
